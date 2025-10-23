527a02a02a7340f585ecf1079767b0b7475c8b39
arielmusa <arielmusainfo@gmail.com>
2025-10-23 19:15:18 +0200

    feat: add MovieService for managing movie data

---
+++ src/main/java/org/lessons/java/spring_backend_movie/service/MovieService.java
@@ -0,0 +1,54 @@
+package org.lessons.java.spring_backend_movie.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.lessons.java.spring_backend_movie.model.Movie;
+import org.lessons.java.spring_backend_movie.model.Review;
+import org.lessons.java.spring_backend_movie.repository.MovieRepository;
+import org.lessons.java.spring_backend_movie.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+    
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    public List<Movie> findAll(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(int id){
+        return movieRepository.findById(id);
+    }
+
+    public List<Movie> findByName(String name){
+        return movieRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public Movie saveMovie(Movie movie){
+        return movieRepository.save(movie);
+    }
+
+    public void delete(Movie movie){
+        for (Review r : movie.getReviews()) {
+            reviewRepository.delete(r);
+        }
+
+        movieRepository.delete(movie);
+    }
+    
+    public void deleteById(int id){
+        Movie movie = findById(id).get();     
+            for (Review r : movie.getReviews()) {
+                reviewRepository.delete(r);
+            }
+            movieRepository.deleteById(id);
+    }
+
+}
