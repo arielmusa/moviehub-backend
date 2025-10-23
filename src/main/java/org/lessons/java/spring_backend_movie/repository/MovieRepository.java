4f84081d22f24f299e37fdba4aaf531561ec0a18
arielmusa <arielmusainfo@gmail.com>
2025-10-23 18:27:19 +0200

    feat: add MovieRepository

---
+++ src/main/java/org/lessons/java/spring_backend_movie/repository/MovieRepository.java
@@ -0,0 +1,8 @@
+package org.lessons.java.spring_backend_movie.repository;
+
+import org.lessons.java.spring_backend_movie.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    
+}
