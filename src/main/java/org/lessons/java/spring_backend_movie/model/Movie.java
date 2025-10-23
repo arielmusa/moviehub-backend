5aeb7a95c82ad286df1fcb83af89aa1360691062
arielmusa <arielmusainfo@gmail.com>
2025-10-23 17:57:47 +0200

    feat: add Movie entity

---
+++ src/main/java/org/lessons/java/spring_backend_movie/model/Movie.java
@@ -0,0 +1,117 @@
+package org.lessons.java.spring_backend_movie.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table (name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Integer id;
+
+    @NotBlank(message = "Movie nave is mandatory")
+    private String name;
+
+    private String director;
+
+    private String genre;
+
+    @Min(value = 1900)
+    private int year;
+
+    @Lob
+    private String description;
+
+    private String image;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Timestamp created_at;
+    
+    private Timestamp updated_at;
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return this.director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Timestamp getCreated_at() {
+        return this.created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return this.updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
+
+}
