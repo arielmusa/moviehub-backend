69042c58b05fe96af918ec5aed841346b9e41e9e
arielmusa <arielmusainfo@gmail.com>
2025-10-23 17:29:47 +0200

    first commit

---
+++ src/main/java/org/lessons/java/spring_backend_movie/SpringBackendMovieApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_backend_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendMovieApplication.class, args);
+	}
+
+}
