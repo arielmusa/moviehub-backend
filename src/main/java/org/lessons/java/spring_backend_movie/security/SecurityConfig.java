9b79af8135c3eb13abc7b399a7530291dad2a64e
arielmusa <arielmusainfo@gmail.com>
2025-10-24 16:44:50 +0200

    feat: add SecurityConfig.
    implement simple filterChain for initial testing and auth provider

---
+++ src/main/java/org/lessons/java/spring_backend_movie/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package org.lessons.java.spring_backend_movie.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+        .requestMatchers("/**").permitAll())
+        .formLogin(Customizer.withDefaults())
+        .logout(Customizer.withDefaults())
+        .exceptionHandling(Customizer.withDefaults());
+        
+        return http.build();
+    }
+
+    @Bean
+    @SuppressWarnings("deprecation")
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    DatabaseUserDetailsService userDetailsService(){
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    
+}
