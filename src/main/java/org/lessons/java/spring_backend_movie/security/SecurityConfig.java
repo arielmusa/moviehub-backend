5373f36d2d9c3674b239afce203065f5279b3531
arielmusa <arielmusainfo@gmail.com>
2025-10-28 23:27:56 +0100

    feat: cors, allow all origins in SecurityConfig

--- src/main/java/org/lessons/java/spring_backend_movie/security/SecurityConfig.java
+++ src/main/java/org/lessons/java/spring_backend_movie/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.lessons.java.spring_backend_movie.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +25,21 @@ public class SecurityConfig {
         .requestMatchers("/**").permitAll())
         .formLogin(Customizer.withDefaults())
         .logout(Customizer.withDefaults())
-        .exceptionHandling(Customizer.withDefaults());
+        .exceptionHandling(Customizer.withDefaults())
+        .cors(cors -> {});
         
         return http.build();
     }
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+	CorsConfiguration configuration = new CorsConfiguration();
+	configuration.setAllowedOrigins(Arrays.asList("*"));
+	configuration.setAllowedMethods(Arrays.asList("*"));
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	source.registerCorsConfiguration("/**", configuration);
+	return source;
+}
 
     @Bean
     @SuppressWarnings("deprecation")
